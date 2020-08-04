01ac084a213021f72064d43ead8ff1b3b3bebf37
Pierce <piercemiller23@gmail.com>
2020-08-04 16:03:05 +1200

    initial commit

---
+++ js/question.js
@@ -0,0 +1,46 @@
+
+
+// Array of Questions
+const quiz = [
+    {
+        q:'Which three typings is grass super effective against?1',
+        options:['<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"> <img src="css/assets/icons/Rock.png" alt="rock">',
+                 '<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"><img src="css/assets/icons/Electric.png" alt="electric">', 
+                 '<img src="css/assets/icons/Rock.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"> <img src="css/assets/icons/Electric.png" alt="rock">', 
+                 '<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Rock.png" alt="ground"> <img src="css/assets/icons/Electric.png" alt="rock">'],
+        answer:0
+    },
+    {
+        q:'Which three typings is grass super effective against?2',
+        options:['<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"> <img src="css/assets/icons/Rock.png" alt="rock">',
+                 '<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"><img src="css/assets/icons/Grass.png" alt="electric">', 
+                 '<img src="css/assets/icons/Rock.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"> <img src="css/assets/icons/Electric.png" alt="rock">', 
+                 '<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Rock.png" alt="ground"> <img src="css/assets/icons/Electric.png" alt="rock">'],
+        answer:2
+    },
+    {
+        q:'Which three typings is grass super effective against?3',
+        options:['<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"> <img src="css/assets/icons/Rock.png" alt="rock">',
+                 '<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"><img src="css/assets/icons/Fire.png" alt="electric">', 
+                 '<img src="css/assets/icons/Rock.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"> <img src="css/assets/icons/Electric.png" alt="rock">', 
+                 '<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Rock.png" alt="ground"> <img src="css/assets/icons/Electric.png" alt="rock">'],
+        answer:3
+    },
+    {
+        q:'Which three typings is grass super effective against?4',
+        options:['<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"> <img src="css/assets/icons/Rock.png" alt="rock">',
+                 '<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"><img src="css/assets/icons/Steel.png" alt="electric">', 
+                 '<img src="css/assets/icons/Rock.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"> <img src="css/assets/icons/Electric.png" alt="rock">', 
+                 '<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Rock.png" alt="ground"> <img src="css/assets/icons/Electric.png" alt="rock">'],
+        answer:3
+    },
+    {
+        q:'Which three typings is grass super effective against?5',
+        options:['<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"> <img src="css/assets/icons/Rock.png" alt="rock">',
+                 '<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"><img src="css/assets/icons/Steel.png" alt="electric">', 
+                 '<img src="css/assets/icons/Rock.png" alt="water"> <img src="css/assets/icons/Ground.png" alt="ground"> <img src="css/assets/icons/Electric.png" alt="rock">', 
+                 '<img src="css/assets/icons/Water.png" alt="water"> <img src="css/assets/icons/Rock.png" alt="ground"> <img src="css/assets/icons/Electric.png" alt="rock">'],
+        answer:4
+    },
+
+]
