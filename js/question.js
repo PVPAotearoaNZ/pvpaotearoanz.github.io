8f3809eb72b5bf1fcc362f235355072d713e04f1
Pierce <piercemiller23@gmail.com>
2020-08-15 22:42:29 +1200

    added 3 more questions

--- js/question.js
+++ js/question.js
@@ -104,39 +104,26 @@ const quiz = [
         options:['True', 'False'],
         answer:0
     },
+    {
+        q:'Golem would be good against Venusaur as <span class="type rock">Rock</span> is super effective against <span class="type grass">Grass</span>',
+        options:['True', 'False'],
+        answer:1
+    },
+    {
+        q:'From the available types, what is <span class="type bug">Bug</span> super effective against?',
+        options:['<img src="css/assets/icons/Grass.png" alt="grass"> <img src="css/assets/icons/Psychic.png" alt="psychic"> <img src="css/assets/icons/dark.png" alt="dark">',
+                 '<img src="css/assets/icons/Psychic.png" alt="psychic"> <img src="css/assets/icons/Normal.png" alt="nomral"> <img src="css/assets/icons/Grass.png" alt="grass">',
+                 '<img src="css/assets/icons/Psychic.png" alt="psychic"> <img src="css/assets/icons/Dark.png" alt="dark"> <img src="css/assets/icons/Ice.png" alt="ice">',
+                 '<img src="css/assets/icons/Normal.png" alt="normal"> <img src="css/assets/icons/Grass.png" alt="grass"> <img src="css/assets/icons/Ice.png" alt="ice">'],
+        answer:0
+    },
+    {
+        q:'From the available types, what is <span class="type psychic">Psychic</span> super effective against?',
+        options:['<img src="css/assets/icons/Fighting.png" alt="fighting"> <img src="css/assets/icons/Poison.png" alt="poison">',
+                 '<img src="css/assets/icons/Psychic.png" alt="psychic"> <img src="css/assets/icons/Poison.png" alt="poison">', 
+                 '<img src="css/assets/icons/Ghost.png" alt="ghost"> <img src="css/assets/icons/Dark.png" alt="dark">', 
+                 '<img src="css/assets/icons/Fighting.png" alt="fighting"> <img src="css/assets/icons/Ghost.png" alt="ghost">'],
+        answer:0
+    },
 ]
 
-//GOOD
-
-/*
-
-
-
-
-What is good against psychic = Fighting,Poison
-What is good against bug = Grass,Psychic,Dark
-What is good against rock = Fire,Ice,Flying,Bug
-*/
-
-//BAD
-
-/*
-What is bad against normal = Rock,Steel
-What is bad against fire = Fire,Water,Rock,Dragon
-What is bad against water = Water,Grass,Dragon
-What is bad against grass = Fire,Grass,Poison,Flying,Bug,Dragon,Steel
-What is bad against electricity =Grass,Electricity,Dragon,
-What is bad against ice = Fire,Water,Ice,Steel
-What is bad against fighting = Poison,Flying,Psychic,Bug,Fairy
-What is bad against poison = Poison,Ground,Rock,Ghost
-What is bad against ground = Grass,Bug
-What is bad against flying = Electricity,Rock,Steel
-What is bad against psychic = Psychic,Steel
-What is bad against bug = Fire,Fighting,Poison,Flying,Ghost,Steel,Fairy
-What is bad against rock = Fighting,Ground,Steel
-What is bad against ghost = Dark,Steel
-What is bad against dragon = Fighting,Dark,Steel,Fairy
-What is bad against steel = Fire,Water,Steel
-What is bad against fairy = Fire,Poison,Steel
-Replace "bad" with "not very effective"
-*/
